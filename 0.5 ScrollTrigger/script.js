35c8147fe4bd43c27e48680cb96c50aed340429d
behan05 <=>
2024-10-20 01:07:24 +0530

    learn basic about ScrollTrigger plugin in GSAP

---
+++ 0.5 ScrollTrigger/script.js
@@ -0,0 +1,57 @@
+// gsap.from("#page1 #box", {
+//     scale: 0,
+//     y: 600,
+//     duration: 1.2,
+//     rotate: 360,
+// })
+
+// gsap.from("#page2 h2", {
+//     opacity: 0,
+//     x: -800,
+//     duration: 2,
+
+//     scrollTrigger: {
+//         trigger: "#page2 h2",
+//         scroller: "body",
+//         start: "top 60%",
+//         end: "top 20%",
+//         markers: true,
+//         toggleActions: "play reverse restart reset",
+//     }
+// })
+// gsap.from("#page2 h1", {
+//     opacity: 0,
+//     x: 800,
+//     duration: 2,
+
+//     scrollTrigger: {
+//         trigger: "#page2 h1",
+//         scroller: "body",
+//         start: "top 70%",
+//         end: "top 20%",
+//         markers: true,
+//         toggleActions: "play reverse restart reset",
+//         // repeat: 2
+//     }
+// })
+
+// gsap.from("#page3 #box", {
+//     scale: 0,
+//     x: 400,
+//     duration: 1.2,
+//     rotate: 360,
+
+//     scrollTrigger: {
+//         trigger: "#page3 #box",
+//         scroller: "body",
+//         start: "top 80%",
+//         end: "top 20%",
+//         scrub: 2,
+//         markers: true,
+//         toggleActions: "play reverse restart reset"
+
+//     }
+// })
+
+
+
